17e47a76e234793fc4f5168a20bf4097e5b431ca
ErichDomme <erich.domme@rwth-aachen.de>
2023-10-10 16:01:14 +0200

    Update TestingPython_script.py

--- LCADNFT.tab/LCADNFT.panel/Step One.pushbutton/TestingPython_script.py
+++ LCADNFT.tab/LCADNFT.panel/Step One.pushbutton/TestingPython_script.py
@@ -1,1 +1,26 @@
-print("Hello World!")
+# Access the current Revit document
+doc = __revit__.ActiveUIDocument.Document
+
+# Create a dictionary to store material data
+material_data = {}
+
+# Traverse through building elements
+for element in FilteredElementCollector(doc).OfCategory(
+    BuiltInCategory.OST_Walls
+):  # Example for walls
+    # Get the material for the element
+    material = element.Material
+    if material:
+        # Extract desired properties (e.g., name, color, texture, etc.)
+        material_name = material.Name
+        material_color = material.Color
+        # ... extract other properties as needed
+
+        # Store the data in the dictionary
+        material_data[material_name] = {
+            "color": material_color,
+            # ... store other properties
+        }
+
+# Print or return the material_data for further processing
+print(material_data)
