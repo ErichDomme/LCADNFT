e5f136a6527c68bd3aac30fa1a50b3e49af762b0
ErichDomme <erich.domme@rwth-aachen.de>
2023-10-10 18:17:29 +0200

    Update TestingPython_script.py

--- LCADNFT.tab/LCADNFT.panel/Testing.pulldown/TestingPython.pushbutton/TestingPython_script.py
+++ LCADNFT.tab/LCADNFT.panel/Testing.pulldown/TestingPython.pushbutton/TestingPython_script.py
@@ -9,8 +9,8 @@ def get_ethereum_price():
 
 
 def get_gas_price():
-    # Make sure to get an API key from Etherscan and replace 'YOUR_API_KEY' with your key
-    url = f"https://api.etherscan.io/api?module=proxy&action=eth_gasPrice&apikey=23FFET6VUG68K6YVED9RF6P79BAU8KV4ZV"
+    # Use your Etherscan API key here
+    url = "https://api.etherscan.io/api?module=proxy&action=eth_gasPrice&apikey=23FFET6VUG68K6YVED9RF6P79BAU8KV4ZV"
     response = requests.get(url)
     data = response.json()
     gas_price_wei = int(data["result"], 16)
@@ -22,5 +22,5 @@ if __name__ == "__main__":
     eth_price = get_ethereum_price()
     gas_price = get_gas_price()
 
-    print(f"Current Ethereum Price: ${eth_price}")
-    print(f"Gas Price: {gas_price} Gwei")
+    print("Current Ethereum Price: $%s" % eth_price)
+    print("Gas Price: %s Gwei" % gas_price)
