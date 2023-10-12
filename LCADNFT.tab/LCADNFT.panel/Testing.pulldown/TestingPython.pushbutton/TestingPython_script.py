1f14befd5561874c2e8df3f3ccd28ac9686b0384
ErichDomme <erich.domme@rwth-aachen.de>
2023-10-12 11:51:26 +0200

    Update TestingPython_script.py

--- LCADNFT.tab/LCADNFT.panel/Testing.pulldown/TestingPython.pushbutton/TestingPython_script.py
+++ LCADNFT.tab/LCADNFT.panel/Testing.pulldown/TestingPython.pushbutton/TestingPython_script.py
@@ -1,28 +1,27 @@
-import clr
-clr.AddReference("System")
-from System import Uri
-from System.Net import WebClient
-import json
+from pyrevit import forms
+import requests
 
-def get_ethereum_price():
-    client = WebClient()
-    url = "https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd"
-    response = client.DownloadString(Uri(url))
-    data = json.loads(response)
-    return data['ethereum']['usd']
+# Define function to fetch Ethereum info
+def get_ethereum_info():
+    API_KEY = "23FFET6VUG68K6YVED9RF6P79BAU8KV4ZV"
+    ETH_PRICE_URL = (
+        f"https://api.etherscan.io/api?module=stats&action=ethprice&apikey={API_KEY}"
+    )
+    GAS_PRICE_URL = f"https://api.etherscan.io/api?module=proxy&action=eth_gasPrice&apikey={API_KEY}"
 
-def get_gas_price():
-    client = WebClient()
-    url = "https://api.etherscan.io/api?module=proxy&action=eth_gasPrice&apikey=23FFET6VUG68K6YVED9RF6P79BAU8KV4ZV"
-    response = client.DownloadString(Uri(url))
-    data = json.loads(response)
-    gas_price_wei = int(data['result'], 16)
-    gas_price_gwei = gas_price_wei / 1e9
-    return gas_price_gwei
+    eth_price_response = requests.get(ETH_PRICE_URL).json()
+    gas_price_response = requests.get(GAS_PRICE_URL).json()
 
-if __name__ == "__main__":
-    eth_price = get_ethereum_price()
-    gas_price = get_gas_price()
+    eth_price = float(eth_price_response["result"]["ethusd"])
+    gas_price_wei = int(gas_price_response["result"], 16)  # Convert hex to int
+    gas_price_gwei = gas_price_wei / 10 ** 9  # Convert wei to gwei
 
-    print "Current Ethereum Price: $%s" % eth_price
-    print "Gas Price: %s Gwei" % gas_price
+    return eth_price, gas_price_gwei
+
+
+# Fetch the info
+eth_price, gas_price = get_ethereum_info()
+
+# Display the results
+message = f"Ethereum Price: ${eth_price}\nGas Price: {gas_price} Gwei"
+forms.alert(message, title="Ethereum Info", ok=True, warn_icon=True)
