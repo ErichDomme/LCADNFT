8c570a06bf4d9f46c9f5e7580b48d1a4603d02aa
ErichDomme <erich.domme@rwth-aachen.de>
2023-10-13 17:22:07 +0200

    Update TestingPython_script.py

--- LCADNFT.tab/LCADNFT.panel/Testing.pulldown/TestingPython.pushbutton/TestingPython_script.py
+++ LCADNFT.tab/LCADNFT.panel/Testing.pulldown/TestingPython.pushbutton/TestingPython_script.py
@@ -1,21 +1,15 @@
 # API_KEY = "23FFET6VUG68K6YVED9RF6P79BAU8KV4ZV"
 
-import sys
-
-print(sys.path)
-
-"""
 from pyrevit import forms
 import clr
 import json
-from config.api_keys import ETHERSCAN_API_KEY
 
 clr.AddReference("System")
 from System.Net import WebClient
 
 # Define function to fetch Ethereum info
 def get_ethereum_info():
-    API_KEY = ETHERSCAN_API_KEY
+    API_KEY = "23FFET6VUG68K6YVED9RF6P79BAU8KV4ZV"
     ETH_PRICE_URL = (
         "https://api.etherscan.io/api?module=stats&action=ethprice&apikey={}".format(
             API_KEY
@@ -42,4 +36,3 @@ eth_price, gas_price = get_ethereum_info()
 # Display the results
 message = "Ethereum Price: ${}\nGas Price: {} Gwei".format(eth_price, gas_price)
 forms.alert(message, title="Ethereum Info", ok=True, warn_icon=True)
-"""
