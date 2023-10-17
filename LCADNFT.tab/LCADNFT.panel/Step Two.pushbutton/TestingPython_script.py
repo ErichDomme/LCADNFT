a2763c81c34c36fa8b66e7d6f851859ce8868438
ErichDomme <erich.domme@rwth-aachen.de>
2023-10-17 12:44:18 +0200

    Update TestingPython_script.py

--- LCADNFT.tab/LCADNFT.panel/Step Two.pushbutton/TestingPython_script.py
+++ LCADNFT.tab/LCADNFT.panel/Step Two.pushbutton/TestingPython_script.py
@@ -1,19 +1,22 @@
-# coding=utf-8
-
-
+# Import necessary Revit API classes
 from Autodesk.Revit.DB import (
-    Options,
     FilteredElementCollector,
     BuiltInCategory,
     Wall,
     Solid,
     ElementId,
+    Options,
 )
 
+# Access the current Revit document
 doc = __revit__.ActiveUIDocument.Document
 
 # Dictionary to store material data with volume and area
 material_data = {}
+
+# Conversion factors
+SQFT_TO_SQM = 0.092903  # Square feet to square meters
+CFT_TO_CBM = 0.0283168  # Cubic feet to cubic meters
 
 # Traverse through building elements (e.g., walls)
 for element in (
@@ -30,6 +33,10 @@ for element in (
             volume = geo_object.Volume
             area = geo_object.SurfaceArea
 
+            # Convert from square feet to square meters and from cubic feet to cubic meters
+            volume_m3 = volume * CFT_TO_CBM
+            area_m2 = area * SQFT_TO_SQM
+
             # Get the materials for the element
             material_ids = element.GetMaterialIds(False)
 
@@ -42,8 +49,8 @@ for element in (
                     material_data[material_name] = {"volume": 0, "area": 0}
 
                 # Accumulate volume and area for the material
-                material_data[material_name]["volume"] += volume
-                material_data[material_name]["area"] += area
+                material_data[material_name]["volume"] += volume_m3
+                material_data[material_name]["area"] += area_m2
 
 # Print the material data with volume and area
 for material, data in material_data.items():
