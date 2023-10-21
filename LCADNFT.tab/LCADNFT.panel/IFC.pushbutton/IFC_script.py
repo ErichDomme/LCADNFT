43881723f3ac80668b166e479b58b3ef098278f5
ErichDomme <erich.domme@rwth-aachen.de>
2023-10-21 15:13:53 +0200

    Update IFC_script.py

--- LCADNFT.tab/LCADNFT.panel/IFC.pushbutton/IFC_script.py
+++ LCADNFT.tab/LCADNFT.panel/IFC.pushbutton/IFC_script.py
@@ -3,11 +3,11 @@ import clr
 
 clr.AddReference("RevitAPI")
 clr.AddReference("RevitAPIUI")
+clr.AddReference("System.Windows.Forms")
 
 from Autodesk.Revit.DB import *
 from Autodesk.Revit.UI import *
-
-import System.Windows.Forms as WinForms
+from System.Windows.Forms import FolderBrowserDialog, DialogResult
 
 # Function to export IFC
 def export_to_ifc(doc, export_path):
@@ -19,8 +19,8 @@ def export_to_ifc(doc, export_path):
 # Main function
 def main():
     # Show folder browser dialog to get export path
-    folder_browser = WinForms.FolderBrowserDialog()
-    if folder_browser.ShowDialog() == WinForms.DialogResult.OK:
+    folder_browser = FolderBrowserDialog()
+    if folder_browser.ShowDialog() == DialogResult.OK:
         export_path = folder_browser.SelectedPath
         # Ensure path ends with .ifc
         if not export_path.lower().endswith(".ifc"):
