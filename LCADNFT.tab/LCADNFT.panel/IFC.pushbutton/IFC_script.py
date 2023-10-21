6efa464c64f45667cecdc803b32cb5dd06710916
ErichDomme <erich.domme@rwth-aachen.de>
2023-10-21 18:45:21 +0200

    Update IFC_script.py

--- LCADNFT.tab/LCADNFT.panel/IFC.pushbutton/IFC_script.py
+++ LCADNFT.tab/LCADNFT.panel/IFC.pushbutton/IFC_script.py
@@ -1,26 +1,12 @@
 import clr
 
-
-from System.Windows.Forms import SaveFileDialog, DialogResult
-from System.Drawing import Point
-
 clr.AddReference("RevitAPI")
 clr.AddReference("RevitAPIUI")
 clr.AddReference("System.Windows.Forms")
-clr.AddReference("System.Drawing")
 
 from Autodesk.Revit.DB import IFCExportOptions, IFCVersion, Transaction
 from Autodesk.Revit.UI import TaskDialog
-from System.Windows.Forms import (
-    FolderBrowserDialog,
-    DialogResult,
-    Form,
-    TextBox,
-    Button,
-    Label,
-)
-from System.Drawing import Point
-
+from System.Windows.Forms import FolderBrowserDialog, DialogResult
 
 # Function to export IFC
 def export_to_ifc(doc, export_folder, filename):
@@ -28,41 +14,18 @@ def export_to_ifc(doc, export_folder, filename):
     ifc_options.FileVersion = IFCVersion.IFC2x3
     doc.Export(export_folder, filename, ifc_options)
 
+# Function to get filename from user
+def get_filename_from_user():
+    td = TaskDialog("Filename")
+    td.MainInstruction = "Enter desired filename for the IFC export"
+    td.MainContent = "Please provide a name without extension (.ifc will be added):"
+    td.AddCommandLink(TaskDialogCommandLinkId.CommandLink1, "Proceed with export")
+    td.AddCommandLink(TaskDialogCommandLinkId.CommandLink2, "Cancel export")
 
-# Custom input dialog to get filename
-def input_filename_dialog(default_text="model.ifc"):
-    form = Form()
-    form.Width = 300
-    form.Height = 150
-    form.Text = "Input filename"
-
-    label = Label()
-    label.Text = "Enter filename for the IFC export:"
-    label.Width = 280
-    label.Location = Point(10, 10)
-    form.Controls.Add(label)
-
-    textbox = TextBox()
-    textbox.Text = default_text
-    textbox.Width = 280
-    textbox.Location = Point(10, 40)
-    form.Controls.Add(textbox)
-
-    button = Button()
-    button.Text = "OK"
-    button.Width = 100
-    button.Location = Point(190, 70)
-
-    def button_event(sender, event):
-        form.Close()
-
-    button.Click += button_event
-    form.Controls.Add(button)
-
-    form.ShowDialog()
-
-    return textbox.Text
-
+    res = td.Show()
+    if res == TaskDialogResult.CommandLink1:
+        return td.CommandLinkText
+    return None
 
 # Main function
 def main():
@@ -70,28 +33,23 @@ def main():
     folder_browser = FolderBrowserDialog()
     if folder_browser.ShowDialog() == DialogResult.OK:
         export_folder = folder_browser.SelectedPath
+        filename = get_filename_from_user()
 
-        # Ask user for filename
-        filename = input_filename_dialog()
-
-        # Check if filename ends with ".ifc". If not, append it.
-        if not filename.endswith(".ifc"):
-            filename += ".ifc"
-
-        # Export IFC
-        try:
-            active_doc = __revit__.ActiveUIDocument.Document
-            # Start a transaction
-            with Transaction(active_doc, "IFC Export") as t:
-                t.Start()
-                export_to_ifc(
-                    doc=active_doc, export_folder=export_folder, filename=filename
-                )
-                t.Commit()
-            TaskDialog.Show("Success", "IFC Exported Successfully!")
-        except Exception as e:
-            TaskDialog.Show("Error", "Error exporting IFC: {}".format(str(e)))
-
+        if filename is not None:
+            filename = filename + ".ifc"
+            # Export IFC
+            try:
+                active_doc = __revit__.ActiveUIDocument.Document
+                # Start a transaction
+                with Transaction(active_doc, "IFC Export") as t:
+                    t.Start()
+                    export_to_ifc(
+                        doc=active_doc, export_folder=export_folder, filename=filename
+                    )
+                    t.Commit()
+                TaskDialog.Show("Success", "IFC Exported Successfully!")
+            except Exception as e:
+                TaskDialog.Show("Error", "Error exporting IFC: {}".format(str(e)))
 
 # Run main function
 main()
