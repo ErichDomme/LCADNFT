c8b59719ed4de8ce5d188ae0e4584f329d590ba0
ErichDomme <erich.domme@rwth-aachen.de>
2023-10-21 18:18:24 +0200

    Update IFC_script.py

--- LCADNFT.tab/LCADNFT.panel/IFC.pushbutton/IFC_script.py
+++ LCADNFT.tab/LCADNFT.panel/IFC.pushbutton/IFC_script.py
@@ -6,7 +6,7 @@ clr.AddReference("System.Windows.Forms")
 
 from Autodesk.Revit.DB import IFCExportOptions, IFCVersion, Transaction
 from Autodesk.Revit.UI import TaskDialog
-from System.Windows.Forms import FolderBrowserDialog, DialogResult
+from System.Windows.Forms import FolderBrowserDialog, DialogResult, InputBox
 
 # Function to export IFC
 def export_to_ifc(doc, export_folder, filename):
@@ -21,7 +21,15 @@ def main():
     folder_browser = FolderBrowserDialog()
     if folder_browser.ShowDialog() == DialogResult.OK:
         export_folder = folder_browser.SelectedPath
-        filename = "model.ifc"
+
+        # Ask user for filename
+        filename = InputBox.Show(
+            "Enter filename for the IFC export:", "IFC Filename", "model.ifc"
+        )
+
+        # Check if filename ends with ".ifc". If not, append it.
+        if not filename.endswith(".ifc"):
+            filename += ".ifc"
 
         # Export IFC
         try:
@@ -40,3 +48,47 @@ def main():
 
 # Run main function
 main()
+
+
+# import clr
+
+# clr.AddReference("RevitAPI")
+# clr.AddReference("RevitAPIUI")
+# clr.AddReference("System.Windows.Forms")
+
+# from Autodesk.Revit.DB import IFCExportOptions, IFCVersion, Transaction
+# from Autodesk.Revit.UI import TaskDialog
+# from System.Windows.Forms import FolderBrowserDialog, DialogResult
+
+# # Function to export IFC
+# def export_to_ifc(doc, export_folder, filename):
+#     ifc_options = IFCExportOptions()
+#     ifc_options.FileVersion = IFCVersion.IFC2x3
+#     doc.Export(export_folder, filename, ifc_options)
+
+
+# # Main function
+# def main():
+#     # Show folder browser dialog to get export path
+#     folder_browser = FolderBrowserDialog()
+#     if folder_browser.ShowDialog() == DialogResult.OK:
+#         export_folder = folder_browser.SelectedPath
+#         filename = "model.ifc"
+
+#         # Export IFC
+#         try:
+#             active_doc = __revit__.ActiveUIDocument.Document
+#             # Start a transaction
+#             with Transaction(active_doc, "IFC Export") as t:
+#                 t.Start()
+#                 export_to_ifc(
+#                     doc=active_doc, export_folder=export_folder, filename=filename
+#                 )
+#                 t.Commit()
+#             TaskDialog.Show("Success", "IFC Exported Successfully!")
+#         except Exception as e:
+#             TaskDialog.Show("Error", "Error exporting IFC: {}".format(str(e)))
+
+
+# # Run main function
+# main()
