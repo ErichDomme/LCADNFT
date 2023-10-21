af7f516be6a946bf43f70a39c7d613edacbd7e4c
ErichDomme <erich.domme@rwth-aachen.de>
2023-10-21 15:31:49 +0200

    Update IFC_script.py

--- LCADNFT.tab/LCADNFT.panel/IFC.pushbutton/IFC_script.py
+++ LCADNFT.tab/LCADNFT.panel/IFC.pushbutton/IFC_script.py
@@ -10,10 +10,10 @@ from Autodesk.Revit.UI import *
 from System.Windows.Forms import FolderBrowserDialog, DialogResult
 
 # Function to export IFC
-def export_to_ifc(doc, export_path):
+def export_to_ifc(doc, export_folder, filename):
     ifc_options = IFCExportOptions()
     ifc_options.FileVersion = IFCVersion.IFC2x3
-    doc.Export(export_path, ".ifc", ifc_options)
+    doc.Export(export_folder, filename, ifc_options)
 
 
 # Main function
@@ -21,14 +21,15 @@ def main():
     # Show folder browser dialog to get export path
     folder_browser = FolderBrowserDialog()
     if folder_browser.ShowDialog() == DialogResult.OK:
-        export_path = folder_browser.SelectedPath
-        # Ensure path ends with .ifc
-        if not export_path.lower().endswith(".ifc"):
-            export_path += r"\model.ifc"
+        export_folder = folder_browser.SelectedPath
+        filename = "model.ifc"
+
         # Export IFC
         try:
             active_doc = __revit__.ActiveUIDocument.Document
-            export_to_ifc(doc=active_doc, export_path=export_path)
+            export_to_ifc(
+                doc=active_doc, export_folder=export_folder, filename=filename
+            )
             TaskDialog.Show("Success", "IFC Exported Successfully!")
         except Exception as e:
             TaskDialog.Show("Error", "Error exporting IFC: {}".format(str(e)))
